b478c8c77eea60e2525a49a8a7bb19716b5a1ab3
Neil Brommer <neil@neilbrommer.com>
2018-05-01 17:30:40 -0700

    Add files

---
+++ AutoDark/AutoDark.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace AutoDark {
+	class AutoDark {
+		static int Main(string[] args) {
+			int mode = -1;
+
+			if (args.Length > 0 && args[0].Equals("dark", StringComparison.InvariantCultureIgnoreCase))
+				mode = 0;
+			else if (args.Length > 0 && args[0].Equals("light", StringComparison.InvariantCultureIgnoreCase))
+				mode = 1;
+			else if (args.Length > 0) // there is an argument, but it's invalid
+				return -1;
+
+			RegistryKey k = Registry.CurrentUser.OpenSubKey(
+				"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",
+				true
+			);
+
+			// if no argument, then just toggle
+			if (mode == -1) {
+				string val = k.GetValue("AppsUseLightTheme", 1).ToString();
+				mode = int.Parse(val);
+				if (mode == 1)
+					mode = 0;
+				else
+					mode = 1;
+			}
+
+			if (k != null) {
+				k.SetValue("AppsUseLightTheme", mode, RegistryValueKind.DWord);
+				k.Close();
+			} else
+				return -2;
+
+			return 0;
+		}
+	}
+}
---
+++ AutoDark/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AutoDark")]
+[assembly: AssemblyDescription("Sets/toggles the Windows dark mode setting")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutoDark")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f24cb25a-c684-4ccb-9346-a0535ab405f9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
